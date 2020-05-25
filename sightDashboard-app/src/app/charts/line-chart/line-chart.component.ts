8a5acc850cfd3e07121621a724b0ce50dc4ec1d0
Esrom de Paiva <epaiva@non-cisatrading.com.br>
2020-05-25 19:46:17 -0300

    Add pie chart componet data

--- sightDashboard-app/src/app/charts/line-chart/line-chart.component.ts
+++ sightDashboard-app/src/app/charts/line-chart/line-chart.component.ts
@@ -9,7 +9,14 @@ export class LineChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number[] = [350, 460, 120, 200];
+  pieChartLabels: string[] = ['ZXC Logistics', 'Main st Bakery', 'Acme Hosting'];
+  colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166']
+    }
+  ];
+  pieChartType = 'pie';
   ngOnInit(): void {
   }
-
 }
--- sightDashboard-app/src/app/charts/pie-chart/pie-chart.component.ts
+++ sightDashboard-app/src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,15 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number[] = [350, 460, 120, 200];
+  pieChartLabels: string[] = ['ZXC Logistics', 'Main st Bakery', 'Acme Hosting'];
+  colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166']
+    }
+  ];
+  pieChartType = 'doughnut';
+
   ngOnInit(): void {
   }
 
