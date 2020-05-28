21935c4a00b59c9ff95698b745340e310c533fa6
Esrom de Paiva <epaiva@non-cisatrading.com.br>
2020-05-28 19:14:04 -0300

    Add Line Chart and styles

--- sightDashboard-app/src/app/charts/line-chart/line-chart.component.ts
+++ sightDashboard-app/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,14 @@
+import { from } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  {data: [32, 14, 46, 23, 12, 50], label: 'Sentiment Analysis'},
+  {data: [10, 21, 32, 46, 49, 50], label: 'Image Recognition'},
+  {data: [11, 10, 15, 19, 40, 26], label: 'Forecasting'},
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -9,14 +19,16 @@ export class LineChartComponent implements OnInit {
 
   constructor() { }
 
-  pieChartData: number[] = [350, 460, 120, 200];
-  pieChartLabels: string[] = ['ZXC Logistics', 'Main st Bakery', 'Acme Hosting'];
-  colors: any[] = [
-    {
-      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166']
-    }
-  ];
-  pieChartType = 'pie';
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    Responsive: true,
+    maintainAspectRatio: false
+  };
+  lineChartLegend: true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
   ngOnInit(): void {
   }
+
 }
--- sightDashboard-app/src/app/charts/pie-chart/pie-chart.component.ts
+++ sightDashboard-app/src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,9 +9,9 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
-  pieChartData: number[] = [350, 460, 120, 200];
-  pieChartLabels: string[] = ['ZXC Logistics', 'Main st Bakery', 'Acme Hosting'];
-  colors: any[] = [
+    pieChartData: number[] = [350, 460, 120, 200];
+    pieChartLabels: string[] = ['ZXC Logistics', 'Main st Bakery', 'Acme Hosting'];
+    colors: any[] = [
     {
       backgroundColor: ['#26547c', '#ff6b6b', '#ffd166']
     }
