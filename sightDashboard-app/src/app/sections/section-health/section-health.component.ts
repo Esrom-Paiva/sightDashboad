d66a81543cd50ed90cb0a23baed955375e71397e
Esrom de Paiva <epaiva@non-cisatrading.com.br>
2020-05-29 20:07:59 -0300

    Create server componente

--- sightDashboard-app/src/app/sections/section-health/section-health.component.ts
+++ sightDashboard-app/src/app/sections/section-health/section-health.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Server } from '../../shared/server';
+
+const SAMPLE_SERVERS = [
+  {id: 1, name: 'dev-web', isOnline: true},
+  {id: 2, name: 'dev-back', isOnline: true},
+  {id: 3, name: 'dev-mobile', isOnline: false},
+  {id: 4, name: 'dev-ops', isOnline: true},
+  {id: 5, name: 'dev-full', isOnline: false},
+];
 
 @Component({
   selector: 'app-section-health',
@@ -8,6 +17,8 @@ import { Component, OnInit } from '@angular/core';
 export class SectionHealthComponent implements OnInit {
 
   constructor() { }
+
+  servers: Server[] = SAMPLE_SERVERS;
 
   ngOnInit(): void {
   }
