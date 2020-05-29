7af697386c11784600e5f3c835f5401a9c47fca0
Esrom de Paiva <epaiva@non-cisatrading.com.br>
2020-05-29 18:10:21 -0300

    Create Orders table with mock data

--- sightDashboard-app/src/app/sections/section-orders/section-orders.component.ts
+++ sightDashboard-app/src/app/sections/section-orders/section-orders.component.ts
@@ -1,3 +1,4 @@
+import { Order } from './../../shared/order';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,8 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+  orders: Order[] = [
+    {id: 1, customer:
+      {id: 1, name: 'Main St Bakery', state: 'CO',  email: 'Test@gmail.com'},
+      amount: 230, placed: new Date(2020, 5, 28), fulfilled: new Date(2020, 5, 28)},
+    {id: 2, customer:
+      {id: 2, name: 'Main St Bakery', state: 'CO',  email: 'Test@gmail.com'},
+      amount: 230, placed: new Date(2020, 5, 28), fulfilled: new Date(2020, 5, 28)},
+    {id: 3, customer:
+      {id: 3, name: 'Main St Bakery', state: 'CO',  email: 'Test@gmail.com'},
+      amount: 230, placed: new Date(2020, 5, 28), fulfilled: new Date(2020, 5, 28)},
+    {id: 4, customer:
+      {id: 4, name: 'Main St Bakery', state: 'CO',  email: 'Test@gmail.com'},
+      amount: 230, placed: new Date(2020, 5, 28), fulfilled: new Date(2020, 5, 28)},
+    {id: 5, customer:
+      {id: 5, name: 'Main St Bakery', state: 'CO',  email: 'Test@gmail.com'},
+      amount: 230, placed: new Date(2020, 5, 28), fulfilled: new Date(2020, 5, 28)},
+  ];
 
   ngOnInit(): void {
   }
 
 }
+
