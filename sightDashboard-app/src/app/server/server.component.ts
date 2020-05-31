cd471848fce29aeade3cd56c919b706c88b3f7fa
Esrom de Paiva <epaiva@non-cisatrading.com.br>
2020-05-31 13:15:37 -0300

    Update server button text depending on isOnline state

--- sightDashboard-app/src/app/server/server.component.ts
+++ sightDashboard-app/src/app/server/server.component.ts
@@ -10,9 +10,30 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
   @Input() server: Server;
 
   ngOnInit(): void {
+    this.setServeAction(this.server.isOnline);
+  }
+
+  setServeAction(isOnline: boolean){
+    if (isOnline){
+      this.server.isOnline = true;
+      this.color = '#b3ffd9';
+      this.buttonText = 'Shut Down';
+    }
+    else {
+      this.server.isOnline = false;
+      this.color = '#ffb3b3';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean){
+    this.setServeAction(!onlineStatus);
   }
 
 }
